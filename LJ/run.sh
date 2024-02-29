a9cec4704ef46c538b7c23fb6da26319b842652d
lanafr <lana.frkin@tum.de>
2024-02-29 01:57:02 +0100

    env

--- LJ/run.sh
+++ LJ/run.sh
@@ -1,12 +1,18 @@
-#!/bin/sh
-
+#!/bin/bash -l
 #SBATCH --job-name=gamdplus
-#SBATCH --output=gamdplus-%A.out  # Standard output of the script (Can be absolute or relative path). %A adds the job id to the file name so you can launch the same script multiple times and get d
-#SBATCH --error=gamdplus-%A.err  # Standard error of the script
-#SBATCH --time=0-24:00:00  # Limit on the total run time (format: days-hours:minutes:seconds)
-#SBATCH --gres=gpu:1  # Number of GPUs if needed
-#SBATCH --cpus-per-task=8  # Number of CPUs (Don't use more than 24 per GPU)
+#SBATCH --nodes=1
+#SBATCH --time=1-00:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --export=ALL
+#SBATCH --output="slurm-%j.out"
+#SBATCH --gres=gpu:a100:1
 #SBATCH --mem=20G  # Memory in GB (Don't use more than 126G per GPU)
+
+#erlangen
+
+ml python/pytorch-1.10py3.9
+export PATH="$PATH:/home/hpc/b118bb/b118bb12/.local/bin"
 
 # load python module
 
